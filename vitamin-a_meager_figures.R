701d46f5136dc28a37d6a4460792f5b8d78d3ff6
hbalikci <balikci@uchicago.edu>
2023-08-18 13:48:58 -0500

    Creating combined figure
    I was able to create a function for the figures and combine them into one figure with a single legend. Shortened individual figure titles and created a larger combined figure title. Let me know about the axes.

--- vitamin-a_meager_figures.R
+++ vitamin-a_meager_figures.R
@@ -7,124 +7,86 @@
 # and these are the only files that this script uses
 
 
-library(reshape2) #for melt()
+library(reshape2)
 library(ggplot2)
+library(patchwork)
+library(cowplot)
+library(gridGraphics)
 
-### FIGURE 1: Heat map Normal-Normal Simulations ###
-
-# load in the data with your pathname
-load("simulations_results/meta_analysis_monte_carlo_rubin_grid_if_se_versus_sigma.RData")
-
-# prep table row and column names for all plots
+# prep table row outside of function
 greeks=c(alpha='\u03b1', 
          tau='\u03c4', 
          sigma='\u03c3',
          beta='\u03b2',
          gamma='\u03b3')
 
-col_names_vector <- c("SE : 0-5","5-10","10-15","15-20")
-row_names_vector <- c(paste0(greeks['sigma']," : 0-5"),"5-10","10-15","15-20")
+# Define a function to generate a heatmap plot
+generate_heatmap <- function(data_path, title) {
+  load(data_path)
+  
+  # Organize the data into a matrix of heat values
+  fe_mse_divided_by_bhm_mse <- fe_tau_error/bhm_tau_error
+  row_names_vector <- c(paste0(greeks['sigma'], " : 0-5"), "5-10", "10-15", "15-20")
+  col_names_vector <- c("SE : 0-5", "5-10", "10-15", "15-20")
+  
+  rownames(fe_mse_divided_by_bhm_mse) <- row_names_vector
+  colnames(fe_mse_divided_by_bhm_mse) <- col_names_vector
+  
+  longData <- melt(fe_mse_divided_by_bhm_mse)
 
-### Organise the data into a matrix of heat values ###
+  # Create the heatmap plot
+  ggplot(longData,
+         aes(x = Var2, y = Var1, fill = value)) +
+    geom_tile(aes(fill = value), colour = "wheat") +
+    scale_fill_gradient(low = "wheat", high = "red4") +
+    coord_equal() +
+    ggtitle(title) +
+    labs(fill = "Ratio") +
+    theme_bw()
+}
 
-fe_mse_divided_by_bhm_mse <- fe_tau_error/bhm_tau_error
-rownames(fe_mse_divided_by_bhm_mse) <- row_names_vector
-colnames(fe_mse_divided_by_bhm_mse) <- col_names_vector
+# List of data paths and corresponding titles
+data_paths <- c(
+  "/Users/hbalikci/Documents/Chicago_Harris/Life Admin/DIL/Vitamin A/vitamin-A_dropbox/meta_analysis_monte_carlo_rubin_grid_if_se_versus_sigma.RData",
+  "/Users/hbalikci/Documents/Chicago_Harris/Life Admin/DIL/Vitamin A/vitamin-A_dropbox/meta_analysis_monte_carlo_rubin_grid_of_se_versus_sigma_student_t_16_cells.RData",
+  "/Users/hbalikci/Documents/Chicago_Harris/Life Admin/DIL/Vitamin A/vitamin-A_dropbox/meta_analysis_monte_carlo_rubin_grid_of_se_versus_sigma_location_outlier_16_cells.RData",
+  "/Users/hbalikci/Documents/Chicago_Harris/Life Admin/DIL/Vitamin A/vitamin-A_dropbox/meta_analysis_monte_carlo_rubin_grid_of_se_versus_sigma_precision_outlier_16_cells.RData"
+)
+titles <- c(
+  "Normal-Normal Simulations",
+  "With Student t distributed effects",
+  "With Location Outliers",
+  "With Precision Outliers"
+)
 
-rownames(fe_tau_error) <-  row_names_vector
-colnames(fe_tau_error) <- col_names_vector
-rownames(bhm_tau_error) <-  row_names_vector
-colnames(bhm_tau_error) <- col_names_vector
+# Create figures patchwork without legends to build in larger figure
+heatmap_plots <- lapply(seq_along(data_paths), function(i) {
+  generate_heatmap(data_paths[i], titles[i]) +
+    labs(x = NULL, y = NULL) +
+    theme(legend.position = "none")
+})
 
-longData <- melt(fe_mse_divided_by_bhm_mse)
+# Create figures patchwork with legends to extract
+heatmap_plots_with_legends <- lapply(seq_along(data_paths), function(i) {
+  generate_heatmap(data_paths[i], titles[i]) +
+    labs(x = NULL, y = NULL)
+})
 
-### define palate and make graphic ##
+# Arrange in grid
+grid <- plot_grid(plotlist = heatmap_plots, ncol = 2)
 
-zp1 <- ggplot(longData,
-              aes(x = Var2, y = Var1, fill = value)) +
-              geom_tile(aes(fill = value), colour = "wheat") +
-              scale_fill_gradient(low = "wheat", high = "red4") +
-              coord_equal() +ggtitle("Ratio of FE MSE to BHM MSE in Normal-Normal Simulations") +
-              theme_bw() +
-              xlab("Standard errors of point estimates") +
-              ylab("Standard deviation of true effects")
+# Extract figure 4s legend
+legend <- get_legend(heatmap_plots_with_legends[[4]])
 
+final_plot <- plot_grid(grid, legend, ncol = 2, rel_widths = c(0.8, 0.2))
 
-### FIGURE 2: heat map student t ###
+# Combine the combined plot with a common legend and common labels
+final_plot <- final_plot +
+  plot_annotation(
+    title = "Ratio of FE MSE to BHM MSE in Normal-Normal Simulations",
+    theme = theme(plot.title = element_text(hjust = 0.5))) +
+  theme(legend.title = element_blank()) +
+  labs(fill = "Ratio")
 
-# load in the data with your pathname
-load("simulations_results/meta_analysis_monte_carlo_rubin_grid_of_se_versus_sigma_student_t_16_cells.RData")
-
-
-### Organise the data into a matrix of heat values ###
-
-fe_mse_divided_by_bhm_mse_student_t <- fe_tau_error/bhm_tau_error
-rownames(fe_mse_divided_by_bhm_mse_student_t) <- c("0-5", "5-10","10-15","15-20")
-colnames(fe_mse_divided_by_bhm_mse_student_t) <- c("0-5","5-10","10-15","15-20")
-longData <- melt(fe_mse_divided_by_bhm_mse_student_t)
-
-### define palate and make graphic ##
-zp2 <- ggplot(longData,
-              aes(x = Var2, y = Var1, fill = value)) +
-              geom_tile(aes(fill = value), colour = "wheat") + 
-              scale_fill_gradient(low = "wheat", high = "red4") + 
-              coord_equal() +
-              ggtitle("Ratio of FE MSE to BHM MSE \n in Simulations with Student t distributed effects") + 
-              theme_bw() +
-              xlab("Standard errors of point estimates") +
-              ylab("Standard deviation of true effects")
-
-
-### FIGURE 3: heat map with location outliers ###
-
-# load in the data with your pathname
-load("simulations_results/meta_analysis_monte_carlo_rubin_grid_of_se_versus_sigma_location_outlier_16_cells.RData")
-
-### Organise the data into a matrix of heat values ###
-
-fe_mse_divided_by_bhm_mse_location_outliers <- fe_tau_error/bhm_tau_error
-rownames(fe_mse_divided_by_bhm_mse_location_outliers) <- c("0-5","5-10","10-15","15-20")
-colnames(fe_mse_divided_by_bhm_mse_location_outliers) <- c("0-5","5-10","10-15","15-20")
-
-longData <- melt(fe_mse_divided_by_bhm_mse_location_outliers)
-
-### define palate and make graphic ##
-
-zp3 <- ggplot(longData,
-              aes(x = Var2, y = Var1, fill = value)) +
-              geom_tile(aes(fill = value), colour = "wheat") +
-              scale_fill_gradient(low = "wheat", high = "red4") +
-              coord_equal() +
-              ggtitle("Ratio of FE MSE to BHM MSE \n in Normal-Normal Simulations with Location Outliers") +
-              theme_bw() +
-              xlab("Standard errors of point estimates") +
-              ylab("Standard deviation of true effects")
-
-### FIGURE 4: heat map with precision outliers ###
-
-
-# load in the data with your pathname
-load("simulations_results/meta_analysis_monte_carlo_rubin_grid_of_se_versus_sigma_precision_outlier_16_cells.RData")
-
-### Organise the data into a matrix of heat values ###
-
-fe_mse_divided_by_bhm_mse_precision_outliers <- fe_tau_error/bhm_tau_error
-rownames(fe_mse_divided_by_bhm_mse_precision_outliers) <- c("0-5","5-10","10-15","15-20")
-colnames(fe_mse_divided_by_bhm_mse_precision_outliers) <- c("0-5","5-10","10-15","15-20")
-
-longData <- melt(fe_mse_divided_by_bhm_mse_precision_outliers)
-
-### define palate and make graphic ##
-
-zp4 <- ggplot(longData,
-              aes(x = Var2, y = Var1, fill = value)) +
-  geom_tile(aes(fill = value), colour = "wheat") +
-  scale_fill_gradient(low = "wheat", high = "red4") +
-  coord_equal() +
-  ggtitle("Ratio of FE MSE to BHM MSE \n in Normal-Normal Simulations with precision outliers") +
-  theme_bw() +
-  xlab("Standard errors of point estimates") +
-  ylab("Standard deviation of true effects")
-
-
-
+# Save the final plot
+ggsave("heatmap_plots.png", plot = final_plot)
